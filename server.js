a66a4c82475fe2011a575d72cae738d5f4988183
sasha-2002 <zahaietskij@gmail.com>
2021-04-17 15:53:26 +0300

    some cheks

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ MongoClient.connect(mongoUrl, { useUnifiedTopology: true }).then(function(client
     });
 }).catch(error => console.log(error.message));
 
+
+
+
 app.get('/', function(request, response) {
     response.render('index', { url: "",  m_url: ""});
 });
@@ -32,34 +35,53 @@ app.get('/l', function(request, response) {
     let code = request.query.id;
     console.log("req.query.id ");
     console.log(code);
-    mongo.collection("urls").findOne({url_code: Number(code)}, function(err, doc){
-        console.log(doc);
-        response.redirect(doc.main_url);
+    mongo.collection("urls")
+    .findOne({url_code: Number(code)}, function(err, doc){
+        if(doc) {
+            response.redirect(doc.main_url);
+            
+        }
+        else {
+            response.render('invalid');
+        }
+        
     }); 
+    
 });
 
 app.post('/', urlencodedParser, function (request, response) {
-    counter_global++;
-    mongo.collection("urls").updateOne(
-        {id: "main_counter"}, 
-        { $set: {counter: counter_global}}
-    );
+    if(request.body.input.match(/https?:\/\/[^\s]+/g) != null && request.body.input != '') {
+        counter_global++;
+        mongo.collection("urls").updateOne(
+            {id: "main_counter"}, 
+            { $set: {counter: counter_global}}
+        );
 
-    console.log("code " + counter_global);
-
-    mongo
-    .collection('urls')
-    .insertOne({
-      url_code: counter_global,
-      main_url: request.body.input
-    })
-    .then(function() {
-      console.log('Запис створено');
-    });
+        mongo.collection('urls')
+        .insertOne({
+            url_code: counter_global,
+            main_url: request.body.input
+        })
+        .then(function() {
+            console.log('Запис створено');
+        });
+        
+        response.render('index', { url: "http://localhost:3000/l?id="+counter_global, m_url: request.body.input});
+    }
+    else {
+        response.render('invalid')
+    }
     
-    response.render('index', { url: "http://localhost:3000/l?id="+counter_global, m_url: request.body.input});
-    //response.render('output', { url: "http://localhost:3000/l?id="+counter_global });
 });
+
+
+
+app.get('*', function(req, res) {
+
+    res.render('not_found');
+});
+
+
 
 app.listen(3000, function() {
     console.log('App started on http://localhost:3000');
