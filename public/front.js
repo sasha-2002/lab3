702710d0b5944ad704ec361d217032ff883f596d
sasha-2002 <zahaietskij@gmail.com>
2021-04-18 18:35:32 +0300

    eslint

--- public/front.js
+++ public/front.js
@@ -1,17 +1,21 @@
+/* eslint-disable no-unused-vars */
+/* eslint space-before-blocks: ["error", "never"]*/
 /*
-
-валідація на сервері + наче все
 отримання нормальних кодів а не счетчик
 превірка на багато лінків
-отримання кодів черех /id як в ютубі
-перевірка на наявність силки в базі і якшо є то видати той код на який є силка а не створювати новий
+перевірка на наявність силки в базі і якшо є то видати той код на який \
+є силка а не створювати новий
 
 (\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])
 (https?:\/\/[^\s]+)
-
 */
+/**
+ *
+ * @param {form} form_data
+ */
 function check(form_data){
-    if (form_data.main_input.value != ''){
-        form_data.submit();
-    }
+  if (form_data.main_input.value != ''){
+    form_data.submit();
+  }
 }
+
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongodb = require('mongodb');
-const path = require('path');
+
 const MongoClient = mongodb.MongoClient;
 const mongoUrl = 'mongodb://localhost:27017/lab3';
-const domen = "http://localhost:3000";
+const domen = 'http://localhost:3000';
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 const app = express();
@@ -16,75 +16,62 @@ app.use('/static', express.static(__dirname + '/public'));
 let mongo;
 let counter_global;
 
-MongoClient.connect(mongoUrl, { useUnifiedTopology: true }).then(function(client) {
-    mongo = client.db();
-    mongo.collection("urls").findOne({id: "main_counter"}, function (err, doc){
-        counter_global = doc.counter;
-    });
-}).catch(error => console.log(error.message));
-
-
+MongoClient
+    .connect(mongoUrl, {useUnifiedTopology: true})
+    .then(function(client) {
+      mongo = client.db();
+      mongo
+          .collection('urls')
+          .findOne({id: 'main_counter'}, function(err, doc) {
+            counter_global = doc.counter;
+          });
+    }).catch((error) => console.log(error.message));
 
 
 app.get('/', function(request, response) {
-    response.render('index', { url: "",  m_url: ""});
+  response.render('index', {url: '', m_url: ''});
 });
 
 
-app.get('/l', function(request, response) {
-    
-    let code = request.query.id;
-    console.log("req.query.id ");
-    console.log(code);
-    mongo.collection("urls")
-    .findOne({url_code: Number(code)}, function(err, doc){
-        if(doc) {
-            response.redirect(doc.main_url);
-            
-        }
-        else {
-            response.render('invalid');
-        }
-        
-    }); 
-    
+app.get('/:id', function(request, response) {
+  const code = request.params.id;
+  mongo
+      .collection('urls')
+      .findOne({url_code: Number(code)}, function(err, doc) {
+    doc != null ? response.redirect(doc.main_url) : response.render('invalid');
+      });
 });
 
-app.post('/', urlencodedParser, function (request, response) {
-    if(request.body.input.match(/https?:\/\/[^\s]+/g) != null && request.body.input != '') {
-        counter_global++;
-        mongo.collection("urls").updateOne(
-            {id: "main_counter"}, 
-            { $set: {counter: counter_global}}
-        );
+app.post('/', urlencodedParser, function(request, response) {
+  // eslint-disable-next-line max-len
+  if (request.body.input.match(/https?:\/\/[^\s]+/g) != null && request.body.input != '') {
+    counter_global++;
+    mongo.collection('urls').updateOne(
+        {id: 'main_counter'},
+        {$set: {counter: counter_global}},
+    );
 
-        mongo.collection('urls')
+    mongo.collection('urls')
         .insertOne({
-            url_code: counter_global,
-            main_url: request.body.input
+          url_code: counter_global,
+          main_url: request.body.input,
         })
         .then(function() {
-            console.log('Запис створено');
+          console.log('Запис створено');
         });
-        
-        response.render('index', { url: domen +"/l?id="+counter_global, m_url: request.body.input});
-    }
-    else {
-        response.render('invalid')
-    }
-    
+
+    response.render('index',
+        {url: domen + '/' + counter_global, m_url: request.body.input});
+  } else {
+    response.render('invalid');
+  }
 });
-
-
 
 app.get('*', function(req, res) {
-
-    res.render('not_found');
+  res.render('not_found');
 });
 
-
-
 app.listen(3000, function() {
-    console.log('App started on http://localhost:3000');
+  console.log('App started on '+ domen);
 });
 
